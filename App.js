d4d3cfbe9b83d3be68132dce296d23f401f8c3d3
kinyar <kinnykin.kj@gmail.com>
2024-10-03 01:32:58 +0100

    Created screens (#1)
    - Created the crudler screens
    - Created a reusable component

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import ModuleListScreen from "./src/components/screens/ModuleListScreen";
+import ModuleAddScreen from "./src/components/screens/ModuleAddScreen";
+import ModuleModifyScreen from "./src/components/screens/ModuleModifyScreen";
+import ModuleViewScreen from "./src/components/screens/ModuleViewScreen";
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+
+export const App = () => {
+  // Initialisations -----
+  // State -----
+  // Handlers ----
+  // View ------
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="ModuleAddScreen"
+        screenOptions={{
+          headerStyle: { backgroundColor: "black" },
+          headerTintColor: "white",
+        }}
+      >
+        <Stack.Screen
+          name="ModuleListScreen"
+          component={ModuleListScreen}
+          options={{ title: "List Modules" }}
+        />
+        <Stack.Screen
+          name="ModuleAddScreen"
+          component={ModuleAddScreen}
+          options={{ title: "Add Modules" }}
+        />
+        <Stack.Screen
+          name="ModuleViewScreen"
+          component={ModuleViewScreen}
+          options={{ title: "View Modules" }}
+        />
+        <Stack.Screen
+          name="ModuleModifyScreen"
+          component={ModuleModifyScreen}
+          options={{ title: "Modify Modules" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
