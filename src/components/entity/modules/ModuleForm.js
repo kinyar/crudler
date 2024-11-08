b32d81cd8668e01d942daa28b2536789e34ea55c
kinyar <kinnykin.kj@gmail.com>
2024-11-08 15:53:06 +0000

    modify!
    modify works
    uses the form created
    cancel and back buttons work

--- src/components/entity/modules/ModuleForm.js
+++ src/components/entity/modules/ModuleForm.js
@@ -13,7 +13,7 @@ const defaultModule = {
   ModuleImage: null,
 };
 
-const ModuleForm = ({ onSubmit, onCancel }) => {
+const ModuleForm = ({ originalModule, onSubmit, onCancel }) => {
   // Initialisations ---
   defaultModule.ModuleID = Math.floor(100000 + Math.random() * 900000);
   defaultModule.ModuleImage =
@@ -28,7 +28,7 @@ const ModuleForm = ({ onSubmit, onCancel }) => {
   ];
 
   // State --
-  const [module, setModule] = useState(defaultModule);
+  const [module, setModule] = useState(originalModule || defaultModule);
 
   // Handlers --
   const handleChange = (field, value) =>
@@ -36,8 +36,8 @@ const ModuleForm = ({ onSubmit, onCancel }) => {
   const handleSubmit = () => onSubmit(module);
 
   // View --
-  const submitLabel = "Add";
-  const submitIcon = <Icons.Add />;
+  const submitLabel = originalModule ? "Modify" : "Add";
+  const submitIcon = originalModule ? <Icons.Edit /> : <Icons.Add />;
   return (
     <Form
       onSubmit={handleSubmit}
