2e1ae11f37bcf0580203e8ced1753424aa0e8bdf
kinyar <kinnykin.kj@gmail.com>
2024-11-08 15:53:32 +0000

    modify! (#11)
    modify works
    uses the form created
    cancel and back buttons work

--- src/components/screens/ModuleViewScreen.js
+++ src/components/screens/ModuleViewScreen.js
@@ -4,13 +4,19 @@ import ModuleView from "../entity/modules/ModuleView";
 
 const ModuleViewScreen = ({ navigation, route }) => {
   // Initialisations -----
-  const { module, onDelete } = route.params;
+  const { module, onDelete, onModify } = route.params;
   // State -----
   // Handlers ----
+  const gotoModifyScreen = () =>
+    navigation.navigate("ModuleModifyScreen", { module, onModify });
   // View ------
   return (
     <Screen>
-      <ModuleView module={module} onDelete={onDelete} />
+      <ModuleView
+        module={module}
+        onDelete={onDelete}
+        onModify={gotoModifyScreen}
+      />
     </Screen>
   );
 };
