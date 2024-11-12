15e5a6d58d96eb950c397e35c534c2aa1a5d4291
kinyar <kinnykin.kj@gmail.com>
2024-11-12 03:26:42 +0000

    user crudl
    -Begun creating the user add,view,list,modules
    -Added them to App.js navigation

---
+++ src/components/screens/UserViewScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, Text } from "react-native";
+import Screen from "../layout/Screen";
+
+const UserViewScreen = () => {
+  // Initialisations--
+  // State--
+  // Handlers--
+  // View--
+  return (
+    <Screen>
+      <Text>View</Text>
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default UserViewScreen;
