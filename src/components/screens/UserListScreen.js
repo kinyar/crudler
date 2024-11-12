eb199371cd5c638028bb1db90b39972a03bb5af4
kinyar <kinnykin.kj@gmail.com>
2024-11-12 14:43:14 +0000

    userlist screen (#13)
    - Created the user list screen
    - sets up alerts when a user is selected

--- src/components/screens/UserListScreen.js
+++ src/components/screens/UserListScreen.js
@@ -1,18 +1,44 @@
-import { StyleSheet, Text } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import Screen from "../layout/Screen";
+
+import initialUsers from "../../data/users.js";
 
 const UserListScreen = () => {
   // Initialisations--
+  const users = initialUsers;
   // State--
   // Handlers--
+  const handleSelect = () => alert("Item selected");
   // View--
   return (
     <Screen>
-      <Text>List</Text>
+      <ScrollView style={styles.container}>
+        {users.map((user) => {
+          return (
+            <Pressable key={user.UserFirstname} onPress={handleSelect}>
+              <View style={styles.item}>
+                <Text style={styles.text}>
+                  {user.UserFirstname} {user.UserLastname} {user.UserType}
+                </Text>
+              </View>
+            </Pressable>
+          );
+        })}
+      </ScrollView>
     </Screen>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+  item: {
+    paddingVertical: 15,
+    borderTopWidth: 1,
+    borderColor: "lightgray",
+  },
+  text: {
+    fontSize: 16,
+  },
+});
 
 export default UserListScreen;
